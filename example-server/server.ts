b4d71ef859ebea3d2059e043672742fc93d23031
Jordan Eldredge <jordan@jordaneldredge.com>
2023-03-17 09:08:02 -0700

    Fix demo server

--- example-server/server.ts
+++ example-server/server.ts
@@ -2,13 +2,15 @@ import * as express from "express";
 import { graphqlHTTP } from "express-graphql";
 import Query from "./Query";
 import { buildSchema } from "../src";
+import { glob } from "glob";
 
 async function main() {
   const app = express();
 
   // FIXME: This is relative to the current working directory, not the file, or
   // something more sensible.
-  const schema = await buildSchema("./example-server/**/*.ts");
+  const files = await glob("./example-server/**/*.ts");
+  const schema = buildSchema(files);
 
   app.use(
     "/graphql",
