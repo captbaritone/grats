29ed957518fc941c64a4bb4d18ea23c7386a8baa
Jordan Eldredge <jordan@jordaneldredge.com>
2023-04-04 19:48:08 -0700

    Update page metadata

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -31,8 +31,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Implementation-First GraphQL for TypeScript"
     >
       <HomepageHeader />
       <main>
