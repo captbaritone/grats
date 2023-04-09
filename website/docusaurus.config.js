671518879a8c431017a55e22454c7d6eb0fff824
Jordan Eldredge <jordan@jordaneldredge.com>
2023-04-08 18:09:22 -0700

    Use capt.dev for website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://grats.netlify.app",
+  url: "https://grats.capt.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
