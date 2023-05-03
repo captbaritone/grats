b97cabc72eb66744b4327f1023a44e8c23d76e3c
Jordan Eldredge <jordan@jordaneldredge.com>
2023-05-02 21:00:52 -0700

    Sketch out connection example

---
+++ src/tests/integrationFixtures/connectionExample.ts
@@ -0,0 +1,136 @@
+import { Int } from "../../Types";
+
+/** @gqlType */
+export class Query {
+  /** @gqlField */
+  firstHundredIntegers(args: {
+    first: Int | null;
+    after: string | null;
+  }): FirstHundredIntegersConnection {
+    return new FirstHundredIntegersConnection(args.first, args.after);
+  }
+}
+
+/** @gqlType */
+class FirstHundredIntegersConnection {
+  _max: number = 100;
+
+  /** @gqlField */
+  pageInfo: PageInfo;
+
+  /** @gqlField */
+  edges: FirstHundredIntegersEdge[];
+
+  constructor(public first: number | null, public after: string | null) {
+    const start = parseInt(after || "0", 10);
+    const end = first ? start + first : this._max;
+
+    this.edges = [];
+    for (let i = start; i < end; i++) {
+      this.edges.push(new FirstHundredIntegersEdge(i));
+    }
+
+    // TODO: What about empty connections
+    this.pageInfo = new PageInfo({
+      hasNext: end < this._max,
+      hasPrevious: start > 0,
+      startCursor: this.edges[0].cursor,
+      endCursor: this.edges[this.edges.length - 1].cursor,
+    });
+  }
+}
+
+/** @gqlType */
+class PageInfo {
+  constructor({
+    hasNext,
+    hasPrevious,
+    startCursor,
+    endCursor,
+  }: {
+    hasNext: boolean;
+    hasPrevious: boolean;
+    startCursor: string;
+    endCursor: string;
+  }) {
+    this.hasNextPage = hasNext;
+    this.hasPreviousPage = hasPrevious;
+    this.startCursor = startCursor;
+    this.endCursor = endCursor;
+  }
+  /**
+   * @gqlField
+   * @killsParentOnException
+   */
+  hasNextPage: boolean;
+  /**
+   * @gqlField
+   * @killsParentOnException
+   */
+  hasPreviousPage: boolean;
+
+  /** @gqlField */
+  startCursor: string;
+
+  /** @gqlField */
+  endCursor: string;
+}
+
+/** @gqlType */
+class FirstHundredIntegersEdge {
+  constructor(node: number) {
+    this.node = node;
+    this.cursor = node.toString();
+  }
+
+  /** @gqlField */
+  node: Int;
+
+  /** @gqlField */
+  cursor: string;
+}
+
+function gql(strings: TemplateStringsArray) {
+  return strings[0];
+}
+
+export const query = gql`
+  query {
+    firstTwo: firstHundredIntegers(first: 2) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        cursor
+        node
+      }
+    }
+    secondTwo: firstHundredIntegers(first: 2, after: "2") {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        cursor
+        node
+      }
+    }
+    lastTwo: firstHundredIntegers(first: 2, after: "98") {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        cursor
+        node
+      }
+    }
+  }
+`;
