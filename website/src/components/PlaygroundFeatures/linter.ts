0074f553136980113adc7e42911d6b6aeee3d2eb
Jordan Eldredge <jordan@jordaneldredge.com>
2023-11-02 15:37:46 -0700

    Revert "Revert "Note that we fixed #19" (#27)"
    This reverts commit da332408d3684f37638525b51e418e69a681b85f.

--- website/src/components/PlaygroundFeatures/linter.ts
+++ website/src/components/PlaygroundFeatures/linter.ts
@@ -34,7 +34,7 @@ function buildSchemaResultWithFsMap(fsMap, text, config) {
     allowJs: true,
     baseUrl: "./",
     paths: { grats: [GRATS_PATH] },
-    // lib: ["es2021"],
+    lib: [...fsMap.keys()],
   };
   const host = createVirtualCompilerHost(system, compilerOpts, ts);
 
