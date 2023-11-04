5ac400f98815facc87a69b4ed841b52e843be172
Armando Pérez Marqués <gmandx@gmail.com>
2023-11-03 21:51:17 -0700

    `express-graphql` is deprecated; add and example using its successor, `graphql-http`. (#35)

---
+++ examples/express-graphql-http/models/User.ts
@@ -0,0 +1,21 @@
+import IPerson from "../interfaces/IPerson";
+import Query from "../Query";
+import Group from "./Group";
+
+/** @gqlType User */
+export default class UserResolver implements IPerson {
+  __typename = "User";
+  /** @gqlField */
+  name(): string {
+    return "Alice";
+  }
+  /** @gqlField */
+  groups(): Group[] {
+    return [new Group()];
+  }
+}
+
+/** @gqlField */
+export function allUsers(_: Query): UserResolver[] {
+  return [new UserResolver(), new UserResolver()];
+}
