aa45de72a06bb1af62a97e6cf9efedbc1e6f34ea
Jordan Eldredge <jordan@jordaneldredge.com>
2023-11-07 16:42:26 -0800

    Type context in docs examples

--- website/docs/03-resolvers/snippets/02-resolver-signature-method.grats.ts
+++ website/docs/03-resolvers/snippets/02-resolver-signature-method.grats.ts
@@ -2,7 +2,11 @@
 class User {
   _bestFriendID: number;
   /** @gqlField */
-  bestFriend(args: { order: string }, context): User {
+  bestFriend(args: { order: string }, context: GqlContext): User {
     return context.db.getSortedFriends(this._bestFriendID, args.order);
   }
 }
+
+type GqlContext = {
+  db: { getSortedFriends(id: number, order: string): User };
+};
