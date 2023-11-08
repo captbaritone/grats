04b8f0178c078752713cdd468f37071bf0fad30f
Jordan Eldredge <jordan@jordaneldredge.com>
2023-11-07 16:42:26 -0800

    Type context in docs examples

--- website/docs/03-resolvers/snippets/02-resolver-signature-function.grats.ts
+++ website/docs/03-resolvers/snippets/02-resolver-signature-function.grats.ts
@@ -4,6 +4,14 @@ type User = {
 };
 
 /** @gqlField */
-export function friends(user: User, args: { order: string }, context): User {
+export function friends(
+  user: User,
+  args: { order: string },
+  context: GqlContext,
+): User {
   return context.db.getSortedFriends(user._bestFriendID, args.order);
 }
+
+type GqlContext = {
+  db: { getSortedFriends(id: number, order: string): User };
+};
