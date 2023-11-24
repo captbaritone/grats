029ce65be0d66d20e3476c3651a41d1b5b693956
Jordan Eldredge <jordan@jordaneldredge.com>
2023-11-23 17:21:07 -0800

    Add example using ts-node

---
+++ examples/ts-node/server.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from "fs";
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+
+import { extractGratsSchemaAtRuntime, buildSchemaFromSDL } from "grats";
+
+async function main() {
+  // FIXME: This is relative to the current working directory, not the file, or
+  // something more sensible.
+  const schema = getSchema();
+
+  const yoga = createYoga({
+    schema,
+  });
+
+  const server = createServer(yoga);
+
+  server.listen(4000, () => {
+    console.log(
+      "Running a GraphQL API server at http://localhost:4000/graphql",
+    );
+  });
+}
+
+function getSchema() {
+  if (process.env.FROM_SDL) {
+    console.log("Building schema from SDL...");
+    const sdl = readFileSync("./schema.graphql", "utf8");
+    return buildSchemaFromSDL(sdl);
+  }
+  console.log("Building schema from source...");
+  return extractGratsSchemaAtRuntime({
+    emitSchemaFile: "./schema.graphql",
+  });
+}
+
+main();
