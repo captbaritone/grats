239392ed6982a7dbc8682aeeb4ee610aaa5494b6
Jordan Eldredge <jordan@jordaneldredge.com>
2023-12-06 14:39:47 -0800

    Don't use experimental feature in example project

--- examples/yoga/server.ts
+++ examples/yoga/server.ts
@@ -1,17 +1,30 @@
 import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
-import { schema } from "./schema";
+import { readFileSync } from "node:fs";
+import { buildSchemaFromSDL, extractGratsSchemaAtRuntime } from "grats";
 
 async function main() {
+  const schema = getSchema();
   const yoga = createYoga({ schema });
 
   const server = createServer(yoga);
 
-  console.log("Using prebuilt schema.");
   server.listen(4000, () => {
     console.log(
       "Running a GraphQL API server at http://localhost:4000/graphql",
     );
+  });
+}
+
+function getSchema() {
+  if (process.env.FROM_SDL) {
+    console.log("Building schema from SDL...");
+    const sdl = readFileSync("./schema.graphql", "utf8");
+    return buildSchemaFromSDL(sdl);
+  }
+  console.log("Building schema from source...");
+  return extractGratsSchemaAtRuntime({
+    emitSchemaFile: "./schema.graphql",
   });
 }
 
