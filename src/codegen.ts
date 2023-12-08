e5f4661db05903c0612b7ee9d31752e2cd7d89d3
Jordan Eldredge <jordan@jordaneldredge.com>
2023-12-08 09:04:34 -0800

    Propagate deprecationReason in codegen

--- src/codegen.ts
+++ src/codegen.ts
@@ -4,6 +4,7 @@ import {
   GraphQLEnumType,
   GraphQLEnumValue,
   GraphQLField,
+  GraphQLInputField,
   GraphQLInputObjectType,
   GraphQLInputType,
   GraphQLInterfaceType,
@@ -134,6 +135,16 @@ class Codegen {
     return F.createPropertyAssignment(
       "description",
       F.createStringLiteral(description),
+    );
+  }
+
+  deprecated(
+    obj: GraphQLEnumValue | GraphQLField<unknown, unknown> | GraphQLArgument,
+  ): ts.PropertyAssignment | null {
+    if (!obj.deprecationReason) return null;
+    return F.createPropertyAssignment(
+      "deprecationReason",
+      F.createStringLiteral(obj.deprecationReason),
     );
   }
 
@@ -435,9 +446,10 @@ class Codegen {
     );
   }
 
-  inputFieldConfig(field: GraphQLArgument): ts.Expression {
+  inputFieldConfig(field: GraphQLInputField): ts.Expression {
     return this.objectLiteral([
       this.description(field.description),
+      this.deprecated(field),
       F.createPropertyAssignment("name", F.createStringLiteral(field.name)),
       F.createPropertyAssignment("type", this.typeReference(field.type)),
     ]);
@@ -449,6 +461,7 @@ class Codegen {
   ): ts.ObjectLiteralExpression {
     return this.objectLiteral([
       this.description(field.description),
+      this.deprecated(field),
       F.createPropertyAssignment("name", F.createStringLiteral(field.name)),
       F.createPropertyAssignment("type", this.typeReference(field.type)),
       field.args.length
@@ -488,6 +501,7 @@ class Codegen {
   argConfig(arg: GraphQLArgument): ts.Expression {
     return this.objectLiteral([
       this.description(arg.description),
+      this.deprecated(arg),
       F.createPropertyAssignment("name", F.createStringLiteral(arg.name)),
       F.createPropertyAssignment("type", this.typeReference(arg.type)),
       // TODO: arg.defaultValue seems to be missing for complex objects
@@ -540,6 +554,7 @@ class Codegen {
   enumValue(obj: GraphQLEnumValue): ts.Expression {
     return this.objectLiteral([
       this.description(obj.description),
+      this.deprecated(obj),
       F.createPropertyAssignment("value", F.createStringLiteral(obj.name)),
     ]);
   }
