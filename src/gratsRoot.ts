2f7323fb43a31ed1895b394bbdbc01fba583cc0c
Jordan Eldredge <jordan@jordaneldredge.com>
2024-03-11 11:48:02 -0700

    Remove empty line

--- src/gratsRoot.ts
+++ src/gratsRoot.ts
@@ -6,7 +6,6 @@ import { relative, resolve, join } from "path";
 // To achieve this, Grats annotates the field with a directive that includes
 // the path to the module that contains the resolver. In order to allow those
 // paths to be relative, they must be relative to something that both the build
-
 // step and the runtime can agree on. This path is that thing.
 const gratsRoot = join(__dirname, "../..");
 
