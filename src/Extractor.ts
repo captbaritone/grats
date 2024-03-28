423ecb4ecacde9bfc31ad8149bad0b6095142b0b
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-03-28 10:05:59 +0000

    adding parameter to error function

--- src/Extractor.ts
+++ src/Extractor.ts
@@ -864,7 +864,7 @@ class Extractor {
     }
 
     if (!ts.isAsExpression(node.initializer)) {
-      this.report(node.initializer, E.typeNameInitializeNotExpression())
+      this.report(node.initializer, E.typeNameInitializeNotExpression(node.initializer.getText()))
       return false;
     }
 
