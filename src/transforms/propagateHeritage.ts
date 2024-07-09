fd7b9fc4a5086c6c8456dca59658687c9486693f
Jordan Eldredge <jordan@jordaneldredge.com>
2024-07-09 16:03:00 -0700

    Refactor to avoid leaking internals of TypeContext

--- src/transforms/propagateHeritage.ts
+++ src/transforms/propagateHeritage.ts
@@ -14,6 +14,7 @@ import {
   ObjectTypeDefinitionNode,
 } from "graphql";
 import { NameDefinition, TypeContext } from "../TypeContext";
+import { nullThrows } from "../utils/helpers";
 
 /**
  * If a class or interface extends or implements another class or interface, we
@@ -73,15 +74,8 @@ class HeritagePropagator {
     def: InterfaceTypeDefinitionNode | ObjectTypeDefinitionNode,
     declaration: ts.ClassDeclaration | ts.InterfaceDeclaration,
   ): InterfaceTypeDefinitionNode | ObjectTypeDefinitionNode {
-    const name = declaration.name;
-    if (name == null) {
-      throw new Error("Name is null");
-    }
-    const symbol = this.ctx.checker.getSymbolAtLocation(name);
-    if (symbol == null) {
-      throw new Error("Symbol is null");
-    }
-    const parentTypes = this.getAllParents(symbol);
+    const name = nullThrows(declaration.name);
+    const parentTypes = this.ctx.getAllParentsForName(name);
 
     // Build up fields
     const fieldsMap = new Map<string, FieldDefinitionNode>();
@@ -121,66 +115,6 @@ class HeritagePropagator {
       def.fields == null ? [...parentFields] : [...def.fields, ...parentFields];
 
     return { ...def, interfaces, fields };
-  }
-
-  /*
-   * Walk the inheritance chain and collect all the parent classes and
-   * interfaces.
-   *
-   * NOTE! Recursion order here is important and part of our documented
-   * behavior. We do an ordered breadth-first traversal to ensure that
-   * if a class implements multiple interfaces, and those interfaces each
-   * implement the same field, we'll inherit the field implementation from
-   * the first interface in the list.
-   *
-   * Normally JavaScript/TypeScript avoid this issue by implementing only
-   * _single_ inheritance, but because we allow inheriting fields from
-   * interfaces, and TypeScript allows classes (and interfaces!) to
-   * implement/extend multiple interfaces, we must handle this multi-inheritance
-   * issue.
-   *
-   * The approach taken here is modeled after Python's MRO (Method Resolution
-   * Order) algorithm.
-   *
-   * https://docs.python.org/3/howto/mro.html
-   *
-   * TODO: Actually read that paper and see if that's the approach we want to
-   * take then implement it.
-   */
-  getAllParents(
-    symbol: ts.Symbol,
-    parents: Set<NameDefinition> = new Set(),
-  ): Set<NameDefinition> {
-    if (symbol.declarations == null) {
-      return parents;
-    }
-
-    for (const declaration of symbol.declarations) {
-      if (
-        ts.isClassDeclaration(declaration) ||
-        ts.isInterfaceDeclaration(declaration)
-      ) {
-        if (declaration.heritageClauses != null) {
-          for (const heritageClause of declaration.heritageClauses) {
-            for (const type of heritageClause.types) {
-              const typeSymbol = this.ctx.checker.getSymbolAtLocation(
-                type.expression,
-              );
-              if (typeSymbol != null && typeSymbol.declarations != null) {
-                for (const decl of typeSymbol.declarations) {
-                  const name = this.ctx._declarationToName.get(decl);
-                  if (name != null) {
-                    parents.add(name);
-                  }
-                }
-                this.getAllParents(typeSymbol, parents);
-              }
-            }
-          }
-        }
-      }
-    }
-    return parents;
   }
 
   fieldsForType(name: NameDefinition): readonly FieldDefinitionNode[] {
