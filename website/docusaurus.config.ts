de135e209e5559240034e88ad1588e4959f46cf8
Jordan Eldredge <jordan@jordaneldredge.com>
2025-01-14 22:12:46 -0800

    Update docusaurus
    Summary:
    
    Test Plan:

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -23,6 +23,8 @@ const config: Config = {
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
+  // This causes problems with playground links since the there's no explicit anchors.
+  onBrokenAnchors: "ignore",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
