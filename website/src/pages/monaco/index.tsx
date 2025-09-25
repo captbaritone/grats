f2f54bbbc2f78b304c55d7fccb492044a53607a2
Jordan Eldredge <jordan@jordaneldredge.com>
2025-09-24 19:23:38 -0700

    Color and layout

--- website/src/pages/monaco/index.tsx
+++ website/src/pages/monaco/index.tsx
@@ -1,5 +1,9 @@
 // Based on https://github.com/facebook/hermes/pull/173/files
 import React from "react";
+import { useColorMode } from "@docusaurus/theme-common";
+import Layout from "@theme/Layout";
+import FillRemainingHeight from "@site/src/components/FillRemainingHeight";
+
 const BrowserOnly = require("@docusaurus/BrowserOnly").default;
 
 const CONTENT = `/** @gqlQueryField */
@@ -35,9 +39,8 @@ type User {
 function MonacoEditorComponent(props) {
   // Import Monaco Editor dynamically to avoid SSR issues
   const MonacoEditor = require("react-monaco-editor").default;
-  
-  // const { isDarkTheme } = useThemeContext();
-  const isDarkTheme = true;
+  const { colorMode } = useColorMode();
+  const isDarkTheme = colorMode !== "light";
 
   function onEditorWillMount(monaco) {
     const vsDarkTheme = {
@@ -57,34 +60,56 @@ function MonacoEditorComponent(props) {
   }
 
   return (
-    <div>
-      <h2>TypeScript Code</h2>
-      <MonacoEditor
-        {...props}
-        value={CONTENT}
-        language="typescript"
-        height={500}
-        editorWillMount={onEditorWillMount}
-        theme={isDarkTheme ? "vs-dark" : "vs-light"}
-      />
-      <h2>Generated GraphQL Schema</h2>
-      <MonacoEditor
-        {...props}
-        value={SCHEMA}
-        language="graphql"
-        height={500}
-        editorWillMount={onEditorWillMount}
-        theme={isDarkTheme ? "vs-dark" : "vs-light"}
-      />
-    </div>
+    <FillRemainingHeight minHeight={300}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        {/* <ConfigBar /> */}
+        <div
+          style={{
+            flexGrow: 1,
+            position: "relative",
+            top: 0,
+            left: 0,
+            right: 0,
+            display: "flex",
+            flexDirection: "row",
+            overflow: "scroll",
+          }}
+        >
+          <MonacoEditor
+            {...props}
+            value={CONTENT}
+            language="typescript"
+            height={500}
+            editorWillMount={onEditorWillMount}
+            theme={isDarkTheme ? "vs-dark" : "vs-light"}
+          />
+          <MonacoEditor
+            {...props}
+            value={SCHEMA}
+            language="graphql"
+            height={500}
+            editorWillMount={onEditorWillMount}
+            theme={isDarkTheme ? "vs-dark" : "vs-light"}
+          />
+        </div>
+      </div>
+    </FillRemainingHeight>
   );
 }
 
 function Editor(props) {
   return (
-    <BrowserOnly fallback={<div>Loading Monaco Editor...</div>}>
-      {() => <MonacoEditorComponent {...props} />}
-    </BrowserOnly>
+    <Layout title={`Playground`} noFooter>
+      <BrowserOnly fallback={<div>Loading Monaco Editor...</div>}>
+        {() => <MonacoEditorComponent {...props} />}
+      </BrowserOnly>
+    </Layout>
   );
 }
 
