35b72e13fdab22d5c106942cec778056d70e8ad6
Jordan Eldredge <jordan@jordaneldredge.com>
2025-09-28 12:24:01 -0700

    Progress

--- website/src/components/MonacoPlayground/MonacoPlayground.tsx
+++ website/src/components/MonacoPlayground/MonacoPlayground.tsx
@@ -4,6 +4,21 @@ import { useColorMode } from "@docusaurus/theme-common";
 import FillRemainingHeight from "@site/src/components/FillRemainingHeight";
 import MonacoEditor, { MonacoEditorHandle } from "react-monaco-editor";
 import monaco, { IDisposable } from "monaco-editor";
+import ConfigBar from "./ConfigBar";
+import { ViewMode } from "./types";
+import { TypeScriptWorker } from "@site/src/workerTypes";
+
+/**
+ * # TODO
+ * - [ ] Copy URL
+ * - [ ] Persist state in URL
+ * - [ ] Format button
+ * - [ ] Config options (nullable by default, etc)
+ * - [ ] Metadata view
+ * - [ ] Executable schema
+ */
+
+// monaco.languages.typescript.typescriptDefaults.setCompilerOptions({});
 
 // See https://github.com/microsoft/monaco-editor/pull/3488
 window.MonacoEnvironment = {
@@ -60,17 +75,6 @@ class User {
   }
 }`;
 
-const SCHEMA = `type Query {
-  me: User
-  viewer: User @deprecated(reason: "Please use \`me\` instead.")
-}
-
-"""A user in our kick-ass system!"""
-type User {
-  greeting(salutation: String!): String
-  name: String
-}`;
-
 function MonacoEditorComponent() {
   const { colorMode } = useColorMode();
   const theme = colorMode === "dark" ? "vs-dark" : "vs-light";
@@ -80,8 +84,27 @@ function MonacoEditorComponent() {
   const [editor, setEditor] =
     useState<monaco.editor.IStandaloneCodeEditor | null>(null);
 
+  useEffect(() => {
+    if (editor == null) {
+      return;
+    }
+    function handler() {
+      editor!.layout();
+    }
+
+    window.addEventListener("resize", handler);
+    return () => {
+      window.removeEventListener("resize", handler);
+    };
+  });
+
+  const worker = useWorker(editor);
+
+  const [viewMode, setViewMode] = useState<ViewMode>("sdl");
+
   return (
     <FillRemainingHeight minHeight={300}>
+      <ConfigBar viewMode={viewMode} setViewMode={setViewMode} />
       <div
         style={{
           display: "flex",
@@ -89,7 +112,6 @@ function MonacoEditorComponent() {
           height: "100%",
         }}
       >
-        {/* <ConfigBar /> */}
         <div
           style={{
             flexGrow: 1,
@@ -115,69 +137,84 @@ function MonacoEditorComponent() {
               scrollBeyondLastLine: false,
             }}
           />
-          <Right editor={editor} />
+          <Right
+            editor={editor}
+            viewMode={viewMode}
+            worker={worker}
+            theme={theme}
+          />
         </div>
       </div>
     </FillRemainingHeight>
   );
 }
 
-function Right({ editor }) {
-  const [tab, setTab] = useState("ts");
-  let content;
-  switch (tab) {
+function Right({
+  editor,
+  worker,
+  viewMode,
+  theme,
+}: {
+  editor: monaco.editor.IStandaloneCodeEditor | null;
+  worker: TypeScriptWorker;
+  viewMode: ViewMode;
+  theme: string;
+}) {
+  switch (viewMode) {
     case "ts":
-      content = <TsSchema editor={editor} />;
-      break;
-    case "graphql":
-      content = <GraphQLSchema editor={editor} />;
-      break;
+      return <TsSchema editor={editor} worker={worker} theme={theme} />;
+    case "sdl":
+      return <GraphQLSchema editor={editor} worker={worker} theme={theme} />;
+    case "metadata":
+      return <Metadata editor={editor} worker={worker} theme={theme} />;
     default:
       throw new Error("Ooops");
   }
-
-  return (
-    <div style={{ width: "100%" }}>
-      <div style={{ display: "flex", gap: 10, padding: 10 }}>
-        <a
-          style={{
-            fontWeight: tab === "graphql" ? "bold" : "normal",
-          }}
-          onClick={() => {
-            setTab("graphql");
-          }}
-        >
-          GraphQL Schema
-        </a>
-        <a
-          style={{
-            fontWeight: tab === "ts" ? "bold" : "normal",
-          }}
-          onClick={() => {
-            setTab("ts");
-          }}
-        >
-          TS Schema
-        </a>
-      </div>
-      {content}
-    </div>
-  );
 }
 
 function GraphQLSchema({
   editor,
+  worker,
+  theme,
 }: {
   editor: monaco.editor.IStandaloneCodeEditor | null;
+  worker: TypeScriptWorker;
+  theme: string;
 }) {
-  const ts = useWorkerValue(editor, async (worker) => {
+  const gql = useWorkerValue(editor, worker, async (worker) => {
     return worker.getGraphQLSchema();
   });
   return (
     <MonacoEditor
-      value={ts || "Loading..."}
+      value={gql || "Loading..."}
       language="graphql"
-      // theme={theme}
+      theme={theme}
+      options={{
+        minimap: { enabled: false },
+        scrollBeyondLastLine: false,
+        readOnly: true,
+      }}
+    />
+  );
+}
+
+function Metadata({
+  editor,
+  worker,
+  theme,
+}: {
+  editor: monaco.editor.IStandaloneCodeEditor | null;
+  worker: TypeScriptWorker;
+  theme: string;
+}) {
+  const json = useWorkerValue(editor, worker, async (worker) => {
+    return worker.getResolverSignatures();
+  });
+  return (
+    <MonacoEditor
+      value={json || "Loading..."}
+      language="json"
+      theme={theme}
       options={{
         minimap: { enabled: false },
         scrollBeyondLastLine: false,
@@ -189,17 +226,21 @@ function GraphQLSchema({
 
 function TsSchema({
   editor,
+  worker,
+  theme,
 }: {
   editor: monaco.editor.IStandaloneCodeEditor | null;
+  worker: TypeScriptWorker;
+  theme: string;
 }) {
-  const ts = useWorkerValue(editor, async (worker) => {
+  const ts = useWorkerValue(editor, worker, async (worker) => {
     return worker.getTsSchema();
   });
   return (
     <MonacoEditor
       value={ts || "Loading..."}
       language="typescript"
-      // theme={theme}
+      theme={theme}
       options={{
         minimap: { enabled: false },
         scrollBeyondLastLine: false,
@@ -209,8 +250,36 @@ function TsSchema({
   );
 }
 
+function useWorker(editor: monaco.editor.IStandaloneCodeEditor | null) {
+  const [worker, setWorker] = useState<any | null>(null);
+  useEffect(() => {
+    if (editor == null) {
+      // There is no worker until there is an editor I think
+      return;
+    }
+    let unmounted = false;
+    monaco.languages.typescript
+      .getTypeScriptWorker()
+      .then(async (getWorker) => {
+        if (unmounted) {
+          return;
+        }
+        const worker = await getWorker();
+        if (unmounted) {
+          return;
+        }
+        setWorker(worker);
+      });
+    return () => {
+      unmounted = true;
+    };
+  }, [editor]);
+  return worker;
+}
+
 function useWorkerValue<T>(
   editor: monaco.editor.IStandaloneCodeEditor | null,
+  worker: TypeScriptWorker,
   fn: (worker: any) => Promise<T>,
 ): T | null {
   const [tsSchema, setTs] = useState<T | null>(null);
@@ -218,8 +287,11 @@ function useWorkerValue<T>(
     if (editor == null) {
       return;
     }
-
+    if (worker == null) {
+      return;
+    }
     let unmounted = false;
+
     async function setSchema(worker) {
       const value = await fn(worker);
       if (unmounted) {
@@ -229,32 +301,17 @@ function useWorkerValue<T>(
     }
 
     let disposable: IDisposable | null = null;
-    monaco.languages.typescript
-      .getTypeScriptWorker()
-      .then(async (getWorker) => {
-        if (unmounted) {
-          return;
-        }
-        const model = editor.getModel();
-        if (model == null) {
-          throw new Error("Expected to find model");
-        }
-        const worker = await getWorker();
-        if (unmounted) {
-          return;
-        }
-        setSchema(worker);
-        disposable = editor.onDidChangeModelContent(() => {
-          setSchema(worker);
-        });
-      });
+    setSchema(worker);
+    disposable = editor.onDidChangeModelContent(() => {
+      setSchema(worker);
+    });
     return () => {
       unmounted = true;
       if (disposable) {
         disposable.dispose();
       }
     };
-  }, [editor]);
+  }, [editor, worker]);
   return tsSchema;
 }
 
