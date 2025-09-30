9fa5bdcee6419068d548e73e6b65e78e3734e9dd
Jordan Eldredge <jordan@jordaneldredge.com>
2025-09-29 21:32:29 -0700

    Resizeable

--- website/src/components/MonacoPlayground/Editor.tsx
+++ website/src/components/MonacoPlayground/Editor.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef, forwardRef, useImperativeHandle } from "react";
+import React, {
+  useEffect,
+  useRef,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 import MonacoEditor from "react-monaco-editor";
 import monaco from "monaco-editor";
 
@@ -14,53 +19,51 @@ interface EditorProps {
   onEditorDidMount?: (editor: monaco.editor.IStandaloneCodeEditor) => void;
 }
 
-export const Editor = forwardRef<EditorRef, EditorProps>(({
-  value,
-  language,
-  theme,
-  readOnly = false,
-  onEditorDidMount,
-}, ref) => {
-  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+export const Editor = forwardRef<EditorRef, EditorProps>(
+  ({ value, language, theme, readOnly = false, onEditorDidMount }, ref) => {
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
-  useImperativeHandle(ref, () => ({
-    layout: () => {
-      editorRef.current?.layout();
-    },
-  }));
+    useImperativeHandle(ref, () => ({
+      layout: () => {
+        editorRef.current?.layout();
+      },
+    }));
 
-  useEffect(() => {
-    if (editorRef.current == null) {
-      return;
-    }
-    function handler() {
-      editorRef.current!.layout();
-    }
+    useEffect(() => {
+      if (editorRef.current == null) {
+        return;
+      }
+      function handler() {
+        editorRef.current!.layout();
+      }
 
-    window.addEventListener("resize", handler);
-    return () => {
-      window.removeEventListener("resize", handler);
+      window.addEventListener("resize", handler);
+      return () => {
+        window.removeEventListener("resize", handler);
+      };
+    });
+
+    const handleEditorDidMount = (
+      editor: monaco.editor.IStandaloneCodeEditor,
+    ) => {
+      editorRef.current = editor;
+      onEditorDidMount?.(editor);
     };
-  });
 
-  const handleEditorDidMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
-    editorRef.current = editor;
-    onEditorDidMount?.(editor);
-  };
-
-  return (
-    <MonacoEditor
-      editorDidMount={handleEditorDidMount}
-      value={value}
-      language={language}
-      theme={theme}
-      options={{
-        minimap: { enabled: false },
-        scrollBeyondLastLine: false,
-        readOnly,
-      }}
-    />
-  );
-});
+    return (
+      <MonacoEditor
+        editorDidMount={handleEditorDidMount}
+        value={value}
+        language={language}
+        theme={theme}
+        options={{
+          minimap: { enabled: false },
+          scrollBeyondLastLine: false,
+          readOnly,
+        }}
+      />
+    );
+  },
+);
 
 Editor.displayName = "Editor";
