c6dbb7e6e2379809752ac6abb17318db9675461d
Jordan Eldredge <jordan@jordaneldredge.com>
2025-09-29 21:32:29 -0700

    Resizeable

--- website/src/components/MonacoPlayground/Right.tsx
+++ website/src/components/MonacoPlayground/Right.tsx
@@ -16,44 +16,46 @@ export interface RightRef {
   layout: () => void;
 }
 
-export const Right = forwardRef<RightRef, { viewMode: OutputOption }>(({ viewMode }, ref) => {
-  const props = useMemo<RightProps>(() => {
-    switch (viewMode) {
-      case "typescript":
-        return {
-          transform: (worker) => worker.getTsSchema(),
-          language: "typescript" as const,
-        };
-      case "sdl":
-        return {
-          transform: (worker) => worker.getGraphQLSchema(),
-          language: "graphql" as const,
-        };
-      case "resolverSignatures":
-        return {
-          transform: (worker) => worker.getResolverSignatures(),
-          language: "json" as const,
-        };
-      default:
-        throw new Error("Ooops");
+export const Right = forwardRef<RightRef, { viewMode: OutputOption }>(
+  ({ viewMode }, ref) => {
+    const props = useMemo<RightProps>(() => {
+      switch (viewMode) {
+        case "typescript":
+          return {
+            transform: (worker) => worker.getTsSchema(),
+            language: "typescript" as const,
+          };
+        case "sdl":
+          return {
+            transform: (worker) => worker.getGraphQLSchema(),
+            language: "graphql" as const,
+          };
+        case "resolverSignatures":
+          return {
+            transform: (worker) => worker.getResolverSignatures(),
+            language: "json" as const,
+          };
+        default:
+          throw new Error("Ooops");
+      }
+    }, [viewMode]);
+    const value = useWorkerValue(props.transform);
+    const { colorMode } = useColorMode();
+    const theme = colorMode === "dark" ? "vs-dark" : "vs-light";
+    if (value == null) {
+      return <FallbackRight />;
     }
-  }, [viewMode]);
-  const value = useWorkerValue(props.transform);
-  const { colorMode } = useColorMode();
-  const theme = colorMode === "dark" ? "vs-dark" : "vs-light";
-  if (value == null) {
-    return <FallbackRight />;
-  }
-  return (
-    <Editor
-      ref={ref}
-      value={value}
-      language={props.language}
-      theme={theme}
-      readOnly={true}
-    />
-  );
-});
+    return (
+      <Editor
+        ref={ref}
+        value={value}
+        language={props.language}
+        theme={theme}
+        readOnly={true}
+      />
+    );
+  },
+);
 
 Right.displayName = "Right";
 
