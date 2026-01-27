bfd8f849edf59d5f910019c9d6f513519d6a427b
Jordan Eldredge <jordan@jordaneldredge.com>
2026-01-26 20:23:59 -0800

    Upgrade docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -22,9 +22,14 @@ const config: Config = {
   projectName: "grats", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "throw",
   // This causes problems with playground links since the there's no explicit anchors.
   onBrokenAnchors: "ignore",
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "throw",
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
